82bb6b0b22129202d7a064c183a9bd43e1cf228e
Alexander <alexandrolitus@gmail.com>
2020-02-09 17:49:44 +0700

    Git init

---
+++ GitDemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitDemo2
+//
+//  Created by Александр Литвиненко on 09.02.2020.
+//  Copyright © 2020 Александр Литвиненко. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
